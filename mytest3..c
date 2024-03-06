990ce5171dec78778d478c4cfd42dcbfda36ab5b
SakaiIzumi <550971861@qq.com>
2024-03-06 11:00:21 +0800

    Create mytest3..c

---
+++ mytest3..c
@@ -0,0 +1,16 @@
+# Disallow non-root logins when /etc/nologin exists.
+account    required     pam_nologin.so
+
+# Uncomment and edit /etc/security/access.conf if you need to set complex
+# access limits that are hard to express in sshd_config.
+# account  required     pam_access.so
+fadsfdasfdasfdasfasdfds
+fadsfaasdfasdfasdfasdfasdf
+fadsfdasfdasfdasfasdfds
+fadsfaasdfasdfasdfasdfasdf
+fadsfdasfdasfdasfasdfds
+fadsfaasdfasdfasdfasdfasdf
+fadsfdasfdasfdasfasdfds
+fadsfaasdfasdfasdfasdfasdf
+.eastbuy.com
+fdfadasfdsfdasf
